b69cfad202455f4a27393f159a7cbd62ed6d9fe6
Iborges08 <111398149+Iborges08@users.noreply.github.com>
2023-11-27 11:20:38 -0300

    .

--- hash.py
+++ hash.py
@@ -1,60 +1,11 @@
 
-# from flask import Flask, request, jsonify
-# import mysql.connector
-# import hashlib
-# import datetime
-
-# # Configurações do banco de dados
-# conexao = mysql.connector.connect(
-#      host='localhost',
-#      port='3306',
-#      user='root',
-#      password='12345678',
-#      database='dunice'
-#     )
-
-# app = Flask(__name__)
-
-
-# # Função para gerar um hash de 4 números a partir do IP do cliente
-# def generate_hash(ip):
-#     hash_obj = hashlib.md5(ip.encode())
-#     return hash_obj.hexdigest()[:4]
-
-# # Rota para capturar o clique no link "https://dunice.adv.br/"
-# @app.route('/capturar_clique', methods=['GET'])
-# def capturar_clique_dunice():
-#     try:
-#         ip = request.remote_addr
-#         hash_code = generate_hash(ip)
-#         data_hora = datetime.datetime.now()
-
-#         # Conectar ao banco de dados
-#         conn = conexao
-#         cursor = conn.cursor()
-
-#         # Inserir os dados na tabela de cliques
-#         cursor.execute("INSERT INTO cliques(ip, hash_code, data_hora) VALUES (%s, %s, %s)", (ip, hash_code, data_hora))
-#         conn.commit()
-
-#         # conn.close()
-
-#         return jsonify({"message": "Clique capturado com sucesso!"})
-#     except Exception as e:
-#         return jsonify({"error": str(e)})
-
-# if __name__ == '__main__':
-#     app.run()
-
-
 from flask import Flask, request, jsonify
 import mysql.connector
 import hashlib
 import datetime
 
-# Configurações do banco de dados
 conexao = mysql.connector.connect(
-    host='#.#.#.#',
+    host='localhost',
     port='3306',
     user='root',
     password='12345678',
@@ -63,37 +14,26 @@ conexao = mysql.connector.connect(
 
 app = Flask(__name__)
 
-# Função para gerar um hash de 4 números a partir do IP do cliente
-
-
 def generate_hash(ip):
     hash_obj = hashlib.md5(ip.encode())
     return hash_obj.hexdigest()[:4]
 
-# Rota para capturar o clique no link "https://dunice.adv.br/"
-
-
 @app.route('/capturar_clique', methods=['GET'])
 def capturar_clique_dunice():
     try:
-        # Obter o endereço IP do usuário
-        ip = request.headers.get('X-Forwarded-For', request.remote_addr)
+        ip = request.remote_addr
         hash_code = generate_hash(ip)
         data_hora = datetime.datetime.now()
 
-        # Conectar ao banco de dados
         conn = conexao
         cursor = conn.cursor()
 
-        # Inserir os dados na tabela de cliques
-        cursor.execute(
-            "INSERT INTO cliques(ip, hash_code, data_hora) VALUES (%s, %s, %s)", (ip, hash_code, data_hora))
+        cursor.execute("INSERT INTO cliques(ip, hash_code, data_hora) VALUES (%s, %s, %s)", (ip, hash_code, data_hora))
         conn.commit()
 
         return jsonify({"message": "Clique capturado com sucesso!"})
     except Exception as e:
         return jsonify({"error": str(e)})
-
 
 if __name__ == '__main__':
     app.run()
